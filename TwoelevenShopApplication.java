10bd55038f404c652d684b97cae50a1797f0cacc
dispersion <anyta1901@gmail.com>
2022-03-28 00:46:22 +0300

    Initial commit

---
+++ TwoelevenShopApplication.java
@@ -0,0 +1,13 @@
+package com.any.twoelevenshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoelevenShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoelevenShopApplication.class, args);
+    }
+
+}
